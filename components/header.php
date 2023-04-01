4bfb4751619fa88433137d7433d9620a17ddb1cb
catherinepb <86409463+catherinerp@users.noreply.github.com>
2023-04-01 12:32:24 +1100

    added css and top nav

--- components/header.php
+++ components/header.php
@@ -2,8 +2,21 @@
 Author: Catherine Pe Benito
 Created: 06/03/2023
 -->
-<!DOCTYPE HTML PUBLIC>
-<html lang="en">
-<head>
-    <!--<link rel="stylesheet" href=".../assets/css/style.css">-->
-</head>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+	<body>
+		<div class="topnav">
+            <a class="active" href="home.php" target="view">Home</a>
+            <a href="categories.php" target="view">Categories</a>
+            <a href="register.php" target="view">Register</a>
+            <div class="topnav-right">
+                <a href="search.php">Search</a>
+                <a href="#about">About</a>
+            </div>
+        </div>
+        
+	</body>
+</html>	
--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <!-- 
 Author: Catherine Pe Benito
 Created: 06/03/2023
+This page contains the views for different sections of the main webpage.
 -->
-<!DOCTYPE HTML>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <title>Grocery Store</title>
+  <title>GroceryShopping</title>
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+<?php include_once 'components/header.php' ?>
+    <table border=0 width="100%">
+        <tr valign="top">	
+            <td width="10%">
+                <a href="home.php" target="view">
+                    <p>Home</p>
+                </a>
+                <br>
+                <a href="search.html" target="view">
+                    <p>Search</p>
+                </a>
+                <br>
+                <a href="register.html" target="view">
+                    <p>Register</p>
+                </a>
+                <br>
+            </td>
+            <td width="90%">
+                <iframe name="view" src="home.php" frameborder=0 width="100%" height="100%"></iframe>
+            </td>
+        </tr>
+    </table>	
 </body>
+
 </html>
