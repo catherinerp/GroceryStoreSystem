1d2297819e2a9f21b4f1a20bd2c45c343ad50fd5
catherinepb <86409463+catherinerp@users.noreply.github.com>
2023-04-01 12:53:49 +1100

    added banner and logo

--- components/header.php
+++ components/header.php
@@ -8,6 +8,9 @@ Created: 06/03/2023
         <link rel="stylesheet" href="assets/css/style.css">
     </head>
 	<body>
+        <div class="banner">
+            <img class="img-logo" src="assets/images/logo-smallsize.png" alt="Grocery To-Go Logo">
+        </div>
 		<div class="topnav">
             <a class="active" href="home.php" target="view">Home</a>
             <a href="categories.php" target="view">Categories</a>
