1c40993a12e69cccc18dd0be0b9ecda6f3298d07
catherinepb <86409463+catherinerp@users.noreply.github.com>
2023-04-26 17:18:04 +1000

    update cart functionality doesnt work

---
+++ components/updateCart.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+if (isset($_GET['item_id']) && isset($_GET['quantity'])) {
+  $item_id = $_GET['item_id'];
+  $quantity = $_GET['quantity'];
+
+  // Update the quantity in the session cart
+  if ($quantity > 0) {
+    $_SESSION['cart'][$item_id] = $quantity;
+  } else {
+    unset($_SESSION['cart'][$item_id]);
+  }
+}
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+exit();
+?>
